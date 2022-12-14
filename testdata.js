39fdcd7832c6d9dbeeca683a5d578cc2537b4c21
Mike Seiler <snissn@gmail.com>
2022-12-14 23:37:12 +0000

    bugfix

--- testdata.js
+++ testdata.js
@@ -73,7 +73,7 @@ const propcid = 1
 const chanid = ""
 const ContentCreateBody = {} 
 const pubkey = "CAESIKFRR4yzxwk1aKotoPiUtR8OeQ/yydF18J9h5Y8gLAFN"
-const addresses = "/ip4/127.0.0.1/tcp/4001/p2p/12D3KooWLg5h6dARgiBcY6yTTMw8ivPzJufjEBDXahJNKWRGMWNg",
+const addresses = "/ip4/127.0.0.1/tcp/4001/p2p/12D3KooWLg5h6dARgiBcY6yTTMw8ivPzJufjEBDXahJNKWRGMWNg"
 
 const dealIDs = [] 
 
